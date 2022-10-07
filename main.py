a52f1b5f9eca553df3666c8a082e440a5181c94e
aalteirac <anthony@alteirac.com>
2022-10-07 16:15:27 +0200

    wheel sample added

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import streamlit as st
 import streamlit_highcharts as hg
 import pprint
 
+
 st.write("## Example")
-selSample=st.selectbox("Choose a sample",[hg.SAMPLE,hg.SAMPLE2,hg.SAMPLE3,hg.SAMPLE4,hg.SAMPLE5,hg.SAMPLE6,hg.SAMPLE7,hg.SAMPLE8,hg.SAMPLE9,hg.SAMPLE10],format_func=lambda x: str(x["title"]["text"])
+selSample=st.selectbox("Choose a sample",[hg.SAMPLE11,hg.SAMPLE,hg.SAMPLE2,hg.SAMPLE3,hg.SAMPLE4,hg.SAMPLE5,hg.SAMPLE6,hg.SAMPLE7,hg.SAMPLE8,hg.SAMPLE9,hg.SAMPLE10],format_func=lambda x: str(x["title"]["text"])
 )
 value = hg.streamlit_highcharts(selSample,640)
 with st.expander("Show code...",expanded=False):
